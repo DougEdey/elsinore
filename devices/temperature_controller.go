2e7deb38a6174ade227f76d6b8f73c8e313fe84c
Doug Edey <dougedey@fastmail.com>
2021-03-05 21:38:13 +0800

    Add some basic mathtop calculate the average temperatures for a probe

--- devices/temperature_controller.go
+++ devices/temperature_controller.go
@@ -16,7 +16,7 @@ var controllers = make(map[string]*TemperatureController)
 type TemperatureController struct {
 	Name              string
 	LastReadings      map[time.Time]physic.Temperature
-	TemperatureProbes []TemperatureProbe
+	TemperatureProbes []*TemperatureProbe
 	CoolSettings      PidSettings
 	HeatSettings      PidSettings
 	HysteriaSettings  HysteriaSettings
@@ -65,12 +65,17 @@ func FindTemperatureControllerByName(name string) *TemperatureController {
 	return controller
 }
 
+// ClearControllers reset the map of controllers
+func ClearControllers() {
+	controllers = make(map[string]*TemperatureController)
+}
+
 // CreateTemperatureController Create a new PID controller for the Temperature probe
 // name -> The name of the PID Controller
 // probe -> The probe to associate with the controller
 // If a PID controller exists with the same name, the probe will be added to it (or no-op if it is already assigned)
 // If the passed in TemperatureProbe is associated with a different PID Controller, an error will be returned and no PID controller will be returned
-func CreateTemperatureController(name string, probe TemperatureProbe) (*TemperatureController, error) {
+func CreateTemperatureController(name string, probe *TemperatureProbe) (*TemperatureController, error) {
 	existingControllerForProbe := FindTemperatureControllerForProbe(probe.PhysAddr)
 	controller := FindTemperatureControllerByName(name)
 
@@ -88,4 +93,30 @@ func CreateTemperatureController(name string, probe TemperatureProbe) (*Temperat
 
 	controller.TemperatureProbes = append(controller.TemperatureProbes, probe)
 	return controller, nil
+}
+
+// UpdateOutput updates the temperatures and decides how to control the outputs
+func (c *TemperatureController) UpdateOutput() {
+	if len(c.LastReadings) >= 5 {
+		var oldestKey *time.Time
+		for key := range c.LastReadings {
+			if oldestKey == nil || key.Before(*oldestKey) {
+				oldestKey = &key
+			}
+		}
+		delete(c.LastReadings, *oldestKey)
+	}
+	averageTemp := c.AverageTemperature()
+	c.LastReadings[time.Now()] = averageTemp
+}
+
+// AverageTemperature Calculate the average temperature for a temperature controller over all the probes
+func (c *TemperatureController) AverageTemperature() physic.Temperature {
+	var totalTemp int64
+	for _, probe := range c.TemperatureProbes {
+		log.Printf("%v: %v", totalTemp, probe.Reading.Celsius())
+		totalTemp += (int64)(probe.Reading)
+	}
+
+	return (physic.Temperature)(totalTemp / (int64)(len(c.TemperatureProbes)))
 }
--- devices/temperature_controller_test.go
+++ devices/temperature_controller_test.go
@@ -5,16 +5,18 @@ import (
 
 	"github.com/dougedey/elsinore/devices"
 	"periph.io/x/periph/conn/onewire"
+	"periph.io/x/periph/conn/physic"
 )
 
 func TestCreateTemperatureController(t *testing.T) {
+	devices.ClearControllers()
 	probe := devices.TemperatureProbe{
 		PhysAddr: "ARealAddress",
 		Address:  onewire.Address(12345),
 	}
 
 	t.Run("A new Temperature controller is created if no existing device with the same name exists", func(t *testing.T) {
-		temperatureController, err := devices.CreateTemperatureController("sample", probe)
+		temperatureController, err := devices.CreateTemperatureController("sample", &probe)
 		if err != nil {
 			t.Fatalf("Failed to create the Temperature Controller: %v", err)
 		}
@@ -29,7 +31,7 @@ func TestCreateTemperatureController(t *testing.T) {
 	})
 
 	t.Run("A Temperature controller cannot be created if the probe is already associated with a controller", func(t *testing.T) {
-		temperatureController, err := devices.CreateTemperatureController("sample_2", probe)
+		temperatureController, err := devices.CreateTemperatureController("sample_2", &probe)
 
 		if err == nil {
 			t.Fatalf("Created a duplicate Temperature controller: %v", temperatureController)
@@ -37,7 +39,7 @@ func TestCreateTemperatureController(t *testing.T) {
 	})
 
 	t.Run("Re-adding a probe to the same temperature controller is a no-op", func(t *testing.T) {
-		temperatureController, err := devices.CreateTemperatureController("sample", probe)
+		temperatureController, err := devices.CreateTemperatureController("sample", &probe)
 
 		if err != nil {
 			t.Fatalf("Failed to do nothing: %v", err)
@@ -46,6 +48,46 @@ func TestCreateTemperatureController(t *testing.T) {
 		existingController := devices.FindTemperatureControllerByName("sample")
 		if temperatureController != existingController {
 			t.Fatalf("Expected %v, but got %v", existingController, temperatureController)
+		}
+	})
+}
+
+func TestTemperatureControllerAverageTemperature(t *testing.T) {
+	devices.ClearControllers()
+	probe := devices.TemperatureProbe{
+		PhysAddr: "ARealAddress",
+		Address:  onewire.Address(12345),
+		Reading:  physic.Temperature(0),
+	}
+	temperatureController, err := devices.CreateTemperatureController("sample", &probe)
+
+	if err != nil {
+		t.Fatalf("Failed to create the controller: %v", err)
+	}
+
+	t.Run("With a single probe, you get the current value", func(t *testing.T) {
+		probe.UpdateTemperature("35C")
+		avgTemperature := temperatureController.AverageTemperature()
+		if float64(35.0) != avgTemperature.Celsius() {
+			t.Fatalf("Expected %v, but got %v", probe.Reading, avgTemperature)
+		}
+	})
+
+	t.Run("With multiple probes, you get an average value", func(t *testing.T) {
+		probe_two := devices.TemperatureProbe{
+			PhysAddr: "AnotherRealAddress",
+			Address:  onewire.Address(123456),
+			Reading:  physic.Temperature(0),
+		}
+		devices.CreateTemperatureController("sample", &probe_two)
+
+		// probe.Reading = new(physic.Temperature)
+		// probe.Reading.Set("35C")
+		probe_two.UpdateTemperature("37C")
+
+		avgTemperature := temperatureController.AverageTemperature()
+		if float64(36.0) != avgTemperature.Celsius() {
+			t.Fatalf("Expected 36C, but got %v", avgTemperature)
 		}
 	})
 }
